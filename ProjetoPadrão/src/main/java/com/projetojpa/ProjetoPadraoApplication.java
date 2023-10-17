d9ebee1540fb367b8075fd9d79bc384485240fb5
Guilherme-14 <132360130+Guilherme-14@users.noreply.github.com>
2023-10-17 16:23:56 -0300

    Add files via upload

---
+++ ProjetoPadrão/src/main/java/com/projetojpa/ProjetoPadraoApplication.java
@@ -0,0 +1,13 @@
+package com.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPadraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPadraoApplication.class, args);
+	}
+
+}
